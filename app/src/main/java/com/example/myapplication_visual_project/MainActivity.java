c5a783c0059f78227faca59272549d7e1ca9135d
cheayoons <wndkd280@daum.net>
2020-06-09 16:00:47 +0900

    commit

--- app/src/main/java/com/example/myapplication_visual_project/MainActivity.java
+++ app/src/main/java/com/example/myapplication_visual_project/MainActivity.java
@@ -3,29 +3,66 @@ package com.example.myapplication_visual_project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+
     LinearLayout baseLayout;
     Button button1;
+    Button last;
+    Button next;
+    /**
+     * 연/월 텍스트뷰
+     */
+    private TextView tvDate;
+    /**
+     * 그리드뷰 어댑터
+     */
+    private GridAdapter gridAdapter;
 
+    /**
+     * 일 저장 할 리스트
+     */
+    private ArrayList<String> dayList;
+
+    /**
+     * 그리드뷰
+     */
+    private GridView gridView;
+
+    /**
+     * 캘린더 변수
+     */
+    private Calendar mCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         setTitle("달력");
 
@@ -33,8 +70,64 @@ public class MainActivity extends AppCompatActivity {
         button1 = (Button) findViewById(R.id.button1);
         registerForContextMenu(button1);
 
+        tvDate = (TextView)findViewById(R.id.tv_date);
+        gridView = (GridView)findViewById(R.id.gridview);
+
+
+
+        last = (Button) findViewById(R.id.last) ;
+        next = (Button) findViewById(R.id.next) ;
+
+        // 오늘에 날짜를 세팅 해준다.
+        long now = System.currentTimeMillis();
+        final Date date = new Date(now);
+        //연,월,일을 따로 저장
+        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+
+        //현재 날짜 텍스트뷰에 뿌려줌
+        tvDate.setText(curYearFormat.format(date) + "/" + curMonthFormat.format(date) + "/" + curDayFormat.format(date));
+
+        //gridview 요일 표시
+        dayList = new ArrayList<String>();
+        dayList.add("일");
+        dayList.add("월");
+        dayList.add("화");
+        dayList.add("수");
+        dayList.add("목");
+        dayList.add("금");
+        dayList.add("토");
+
+        mCal = Calendar.getInstance();
+
+        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
+        mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
+        int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
+        //1일 - 요일 매칭 시키기 위해 공백 add
+        for (int i = 1; i < dayNum; i++) {
+            dayList.add("");
+        }
+        setCalendarDate(mCal.get(Calendar.MONTH) + 1);
+
+        gridAdapter = new GridAdapter(getApplicationContext(), dayList);
+        gridView.setAdapter(gridAdapter);
+
+
+
+
+        last.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
 
     }
+
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
@@ -47,39 +140,18 @@ public class MainActivity extends AppCompatActivity {
 
             mInflater.inflate(R.menu.menu1, menu);
         }
+
+
     }
 
 
 
- /*
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater mInflater = getMenuInflater();
-        //mInflater.inflate(R.menu.menu1, menu);
-
-        if ( v == button1) {
-            menu.setHeaderTitle("s");
-
-            mInflater.inflate(R.menu.menu1, menu);
-        }
-
-
-        //MenuInflater inflater = getMenuInflater();
-
-        //inflater.inflate(R.menu.menu1, menu);
-        return true;
-    }
-
-
-
-  */
 
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(MenuItem item) {
         //super.onOptionsItemSelected(item);
 
         Toast toast = Toast.makeText(getApplicationContext(),"", Toast.LENGTH_LONG);
@@ -88,16 +160,17 @@ public class MainActivity extends AppCompatActivity {
             case R.id.month:
 
                 toast.setText("month");
-
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
 
                 break;
 
 
 
             case R.id.week:
-                toast.setText("지도");
-                Intent homeIntent = new Intent(this, weekactivity.class);
-                startActivity(homeIntent);
+                toast.setText("week");
+                Intent homeIntents = new Intent(this, weekactivity.class);
+                startActivity(homeIntents);
 
                 break;
         }
@@ -107,7 +180,92 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * 해당 월에 표시할 일 수 구함
+     *
+     * @param month
+     */
+    private void setCalendarDate(int month) {
+        mCal.set(Calendar.MONTH, month - 1);
+
+        for (int i = 0; i < mCal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            dayList.add("" + (i + 1));
+        }
+
+
+
+
+    }
+
+
+
+    /**
+     * 그리드뷰 어댑터
+     *
+     */
+    private class GridAdapter extends BaseAdapter {
+
+        private final List<String> list;
+
+        private final LayoutInflater inflater;
+
+        /**
+         * 생성자
+         *
+         * @param context
+         * @param list
+         */
+        public GridAdapter(Context context, List<String> list) {
+            this.list = list;
+            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
+                holder = new ViewHolder();
+
+                holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+            holder.tvItemGridView.setText("" + getItem(position));
+
+            //해당 날짜 텍스트 컬러,배경 변경
+            mCal = Calendar.getInstance();
+            //오늘 day 가져옴
+            Integer today = mCal.get(Calendar.DAY_OF_MONTH);
+            String sToday = String.valueOf(today);
+            if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
+                holder.tvItemGridView.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView tvItemGridView;
+    }
 
 }
-
-
--- app/src/main/java/com/example/myapplication_visual_project/weekactivity.java
+++ app/src/main/java/com/example/myapplication_visual_project/weekactivity.java
@@ -2,13 +2,85 @@ package com.example.myapplication_visual_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class weekactivity extends AppCompatActivity {
+
+    LinearLayout baseLayout;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekactivity);
+        setTitle("달력");
+
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        button1 = (Button) findViewById(R.id.button1);
+        registerForContextMenu(button1);
+
+
+
     }
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater mInflater = getMenuInflater();
+
+        if ( v == button1) {
+            //menu.setHeaderTitle("s");
+
+            mInflater.inflate(R.menu.menu1, menu);
+        }
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //super.onOptionsItemSelected(item);
+
+        Toast toast = Toast.makeText(getApplicationContext(),"", Toast.LENGTH_LONG);
+
+        switch (item.getItemId()) {
+            case R.id.month:
+
+                toast.setText("month");
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                startActivity(homeIntent);
+
+                break;
+
+
+
+            case R.id.week:
+                toast.setText("week");
+                Intent homeIntents = new Intent(this, weekactivity.class);
+                startActivity(homeIntents);
+
+                break;
+        }
+
+        toast.show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
